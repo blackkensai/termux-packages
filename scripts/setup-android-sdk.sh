8fe7522e77d08048c7ce916287db6527523b3452
blackkensai <blackkensai@gmail.com>
2019-05-10 17:57:15 +0800

    docker

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -9,6 +9,7 @@ ANDROID_SDK_SHA256=92ffee5a1d98d856634e8b71132e8a95d96c83a63fde1099be3d86df3106d
 ANDROID_NDK_FILE=android-ndk-r${TERMUX_NDK_VERSION}-Linux-x86_64.zip
 ANDROID_NDK_SHA256=0fbb1645d0f1de4dde90a4ff79ca5ec4899c835e729d692f433fda501623257a
 
+apt install -y wget
 if [ ! -d $ANDROID_HOME ]; then
 	mkdir -p $ANDROID_HOME
 	cd $ANDROID_HOME/..
@@ -17,9 +18,10 @@ if [ ! -d $ANDROID_HOME ]; then
 	# https://developer.android.com/studio/index.html#command-tools
 	# The downloaded version below is 26.1.1.:
 	echo "Downloading android sdk..."
-	HTTP_PROXY=http://localhost:1080 curl --fail --retry 3 \
-		-o tools.zip \
-		https://dl.google.com/android/repository/${ANDROID_SDK_FILE}
+	# curl --fail --retry 3 \
+	# 	-o tools.zip \
+	# 	https://dl.google.com/android/repository/${ANDROID_SDK_FILE}
+	HTTP_PROXY=http://localhost:1080 wget -O tools.zip https://dl.google.com/android/repository/${ANDROID_SDK_FILE}
 	echo "${ANDROID_SDK_SHA256} tools.zip" | sha256sum -c -
 	rm -Rf android-sdk
 	unzip -q tools.zip -d android-sdk
@@ -31,8 +33,9 @@ if [ ! -d $NDK ]; then
 	cd $NDK/..
 	rm -Rf $(basename $NDK)
 	echo "Downloading android ndk..."
-	HTTP_PROXY=http://localhost:1080 curl --fail --retry 3 -o ndk.zip \
-		https://dl.google.com/android/repository/${ANDROID_NDK_FILE}
+	# curl --fail --retry 3 -o ndk.zip \
+	# 	https://dl.google.com/android/repository/${ANDROID_NDK_FILE}
+	HTTP_PROXY=http://localhost:1080 wget -O tools.zip https://dl.google.com/android/repository/${ANDROID_NDK_FILE}
 	echo "${ANDROID_NDK_SHA256} ndk.zip" | sha256sum -c -
 	rm -Rf android-ndk-r$TERMUX_NDK_VERSION
 	unzip -q ndk.zip
